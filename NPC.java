eb5d3a7bdd8328cff0e9fdab591d0f85984aa1b7
ceeedric <62527577+ceeedric@users.noreply.github.com>
2020-05-09 23:56:45 -0500

    updated code with skins

--- NPC.java
+++ NPC.java
@@ -1,6 +1,8 @@
         PacketContainer player_info_packet = new PacketContainer(PacketType.Play.Server.PLAYER_INFO);
         UUID uuid = UUID.randomUUID();
         WrappedGameProfile gameProfile = new WrappedGameProfile(uuid, "bomdia");
+        String[] skin = getSkin(args[0], gameProfile);
+        gameProfile.getProperties().put("textures", new WrappedSignedProperty("textures", skin[0], skin[1]));
         PlayerInfoData playerInfoData = new PlayerInfoData(gameProfile, 100, EnumWrappers.NativeGameMode.SURVIVAL,
                 null);
         List<PlayerInfoData> list = new ArrayList<>();
@@ -9,7 +11,7 @@
         player_info_packet.getPlayerInfoDataLists().write(0, list);
 
 
-
+// skin code stolen straight from cmaaxx, thanks.
 
 
 
@@ -30,4 +32,30 @@
 
         pm.broadcastServerPacket(player_info_packet);
         pm.broadcastServerPacket(spawn_packet);
+
+private static String[] getSkin(String name, WrappedGameProfile gameProfile) {
+        try {
+            URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + name);
+            InputStreamReader reader = new InputStreamReader(url.openStream());
+            String uuid = new JsonParser().parse(reader).getAsJsonObject().get("id").getAsString();
+
+            URL url2 = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid
+                    + "?unsigned=false");
+            InputStreamReader reader2 = new InputStreamReader(url2.openStream());
+            JsonObject property = new JsonParser().parse(reader2).getAsJsonObject().get("properties")
+                    .getAsJsonArray().get(0).getAsJsonObject();
+            String texture = property.get("value").getAsString();
+            String signature = property.get("signature").getAsString();
+            return new String[]{texture, signature};
+
+        } catch (Exception e) {
+
+            WrappedGameProfile profile = gameProfile;
+            WrappedSignedProperty property = profile.getProperties().get("textures").iterator().next();
+            String texture = property.getValue();
+            String signature = property.getSignature();
+            return new String[]{texture, signature};
+
+        }
+    }
 
