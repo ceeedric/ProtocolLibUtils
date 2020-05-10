54aa09287bf97eca7a3eae9dccc93dba49e31ea0
ceeedric <62527577+ceeedric@users.noreply.github.com>
2020-05-09 22:05:34 -0500

    Update NPC.java

--- NPC.java
+++ NPC.java
@@ -17,23 +17,16 @@
         PacketContainer spawn_packet = new PacketContainer(PacketType.Play.Server.NAMED_ENTITY_SPAWN);
 
         spawn_packet.getUUIDs().write(0, uuid);
+
         Location loc = p.getLocation();
         spawn_packet.getDoubles().write(0, loc.getX())
                 .write(1, loc.getY())
                 .write(2, loc.getZ());
 
-
-
         spawn_packet.getIntegers().write(0, 1000);
-
 
         spawn_packet.getBytes().write(0, (byte) loc.getYaw());
         spawn_packet.getBytes().write(1, (byte) loc.getPitch());
-
-
-        PacketContainer entity_head_rot = new PacketContainer(PacketType.Play.Server.ENTITY_HEAD_ROTATION);
-        entity_head_rot.getBytes().write(0, (byte) (loc.getYaw() * 256 / 360));
-        entity_head_rot.getIntegers().write(0, 1000);
 
         pm.broadcastServerPacket(player_info_packet);
         pm.broadcastServerPacket(spawn_packet);
