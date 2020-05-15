27d8b8a45c1e1add922fe7cffed7fb6b783baf9a
ceeedric <62527577+ceeedric@users.noreply.github.com>
2020-05-15 09:25:35 -0500

    Update CloseWindow.java

--- PacketType/Play/Server/CloseWindow.java
+++ PacketType/Play/Server/CloseWindow.java
@@ -1,3 +1,3 @@
-// Sent when a player closes a window
+// Sent when a window is forcibly closed.
 PacketContainer close_window = e.getPacket();
 close_window.getIntegers().read(0); // returns window ID
