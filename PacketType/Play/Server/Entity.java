540db8ff8132d6f1896cfdb144fd46e16eb78178
ceeedric <62527577+ceeedric@users.noreply.github.com>
2020-05-17 14:00:43 -0500

    Update Entity.java

--- PacketType/Play/Server/Entity.java
+++ PacketType/Play/Server/Entity.java
@@ -1,5 +1,15 @@
-/* I could not find when this is sent */
+/* This packet is to do with paintings according to PacketWrapper but it does not seem to be working */
+/* When listening to this packet I also could not find when it is sent */
 
-PacketContainer entity = e.getPacket();
-entity.getIntegers().read(0); // Returns entity ID
-entity.getEntityModifier(e.getPlayer().getWorld()).read(0); // Returns entity
+/* I found methods but no idea what they are */
+
+PacketContainer packet = new PacketContainer(PacketType.Play.Server.ENTITY);
+packet.getIntegers().write(0, 929); // entity id?
+packet.getBooleans().write(0, true); // unknown
+packet.getBooleans().write(1, true); // unknown
+packet.getBooleans().write(2, true); // unknown
+packet.getBytes().write(0, (byte) 0); // unknown
+packet.getShorts().write(0, (short) 0); // unknown
+packet.getShorts().write(1, (short) 1); // unknown
+packet.getShorts().write(2, (short) 2); // unknown
+packet.getBytes().write(0, (byte) 1); // unknown
