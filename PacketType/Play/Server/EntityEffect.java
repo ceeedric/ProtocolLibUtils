73783ab036a55dad9b79a7080ec3cecfbba87b3e
ceeedric <62527577+ceeedric@users.noreply.github.com>
2020-05-17 04:21:56 -0500

    Update EntityEffect.java

--- PacketType/Play/Server/EntityEffect.java
+++ PacketType/Play/Server/EntityEffect.java
@@ -10,3 +10,5 @@ entity_effect.getEntityModifier(world).read(0); // returns entity
 entity_effect.getBytes().read(0); // Returns effect ID
 entity_effect.getBytes().read(1); // Returns amplifier, starting from zero, meaning if you write 0 here it will turn into a level 1 effect
 entity_effect.getBytes().read(2); // Whether or not to hide particles, 0 means hidden, 1 means shown
+
+// Effect IDs: https://minecraft.gamepedia.com/Status_effect#List_of_effects
