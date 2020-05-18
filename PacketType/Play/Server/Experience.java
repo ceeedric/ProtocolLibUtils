ed6852f1e0eaec8c2c97c506c0b16ee0fe6fa4ec
ceeedric <62527577+ceeedric@users.noreply.github.com>
2020-05-18 03:04:46 -0500

    Create Experience.java

---
+++ PacketType/Play/Server/Experience.java
@@ -0,0 +1,6 @@
+/* This packet is sent to update the experience bar */
+                
+experience.getFloat().read(0); // Returns experience
+
+experience.getIntegers().read(0); // Total experience
+experience.getIntegers().read(1); // Level
