df0d7080906ef972a99002fcaf0c66bdbd3d2edf
ceeedric <62527577+ceeedric@users.noreply.github.com>
2020-05-18 02:35:47 -0500

    Create EntitySound.java

---
+++ PacketType/Play/Server/EntitySound.java
@@ -0,0 +1,12 @@
+/* Plays a sound effect from an entity */
+                
+
+entity_sound.getSoundEffects().read(0); // Returns Sound
+entity_sound.getSoundCategories().read(0); // Returns Sound Category
+
+entity_sound.getEntityModifier(world).read(0); // Returns Entity
+
+entity_sound.getIntegers().read(0); // Returns entity ID
+
+entity_sound.getFloat().read(0); // Volume, 1.0 being 100%, can be more
+entity_sound.getFloat().read(1); // Pitch, between 0.5 and 2.0
