d1014e834ebf8e1d5a8966f93fdac8cc99561f18
ceeedric <62527577+ceeedric@users.noreply.github.com>
2020-05-18 02:45:09 -0500

    Update EntityStatus.java

--- PacketType/Play/Server/EntityStatus.java
+++ PacketType/Play/Server/EntityStatus.java
@@ -1,6 +1,5 @@
 /* Entity statuses generally trigger an animation for an entity */
 /* https://wiki.vg/Entity_statuses */
 
-PacketContainer packet = new PacketContainer(PacketType.Play.Server.ENTITY_STATUS);
-packet.getIntegers().read(0); // Entity ID
-packet.getBytes().read(0); // Status ID, check wiki
+entity_status.getIntegers().read(0); // Entity ID
+entity_status.getBytes().read(0); // Status ID, check wiki
