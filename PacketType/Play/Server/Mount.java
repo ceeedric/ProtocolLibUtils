c2ed1cfc323287958c0320b14256f1dde04acfd5
ceeedric <62527577+ceeedric@users.noreply.github.com>
2020-05-21 15:06:08 -0500

    Create Mount.java

---
+++ PacketType/Play/Server/Mount.java
@@ -0,0 +1,5 @@
+/* This packet is sent every time you mount or unmount an entity */
+                
+mount.getIntegers().read(0); // Entity ID
+mount.getEntityModifier(world).read(0); // Entity
+mount.getIntegerArrays().read(0); // Passenger IDs
