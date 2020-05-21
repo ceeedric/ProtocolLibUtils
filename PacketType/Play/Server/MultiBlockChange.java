edbc1ebcba06b82fb75be4ac0b57b94e08569374
ceeedric <62527577+ceeedric@users.noreply.github.com>
2020-05-21 15:14:45 -0500

    Create MultiBlockChange.java

---
+++ PacketType/Play/Server/MultiBlockChange.java
@@ -0,0 +1,6 @@
+/* Fired whenever 2 or more blocks are changed within the same chunk on the same tick. */
+
+multi_block_change.getChunkCoordIntPairs().read(0); // Returns Chunk
+multi_block_change.getMultiBlockChangeInfoArrays().read(0);
+/* ^ Retrieve a copy of the record data as a block change array. */
+
