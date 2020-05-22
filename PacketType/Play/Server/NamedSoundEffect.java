bb8919501892c6a3ba6dbec1f6af4b35ab0a5156
ceeedric <62527577+ceeedric@users.noreply.github.com>
2020-05-22 08:25:14 -0500

    Create NamedSoundEffect.java

---
+++ PacketType/Play/Server/NamedSoundEffect.java
@@ -0,0 +1,11 @@
+/* Used to play a sound effect on the client */
+
+named_sound_effect.getSoundEffects().read(0); // Returns Sound
+named_sound_effect.getSoundCategories().read(0); // Sound category
+
+named_sound_effect.getIntegers().read(0); // Effect position X
+named_sound_effect.getIntegers().read(1); // Effect position Y
+named_sound_effect.getIntegers().read(2); // Effect position Z
+
+named_sound_effect.getFloat().read(0); // Volume
+named_sound_effect.getFloat().read(1); // Pitch
